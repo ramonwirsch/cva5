f0185b279290f04a03975ef1d884f93ff6fb5ae5
Ramon Wirsch <ramon.wirsch@gmail.com>
2023-09-15 18:55:03 +0200

    Updated fp_mac to be more clear & fixed a tiny advance bug. - removed mul-input buffers
     ! tried structuring to a shared result reg instead of one after each operator. But ends up costing frequency some where.

--- fpu/fp_mac_unit_sp.sv
+++ fpu/fp_mac_unit_sp.sv
@@ -15,8 +15,9 @@ module fp_mac_unit_sp
 
     logic input_advance;
     logic mul_advance;
-    logic addIn_advance; // stage0 of add
-    logic add_advance; // stage1-2 of add
+    logic signMod_advance; // stage0 of add
+    logic add_advance; // further stages of add
+    logic result_advance;
 
     typedef struct packed {
         fp_mac_op_t op;
@@ -26,20 +27,38 @@ module fp_mac_unit_sp
     logic valid_input;
     fp_mac_inputs_t input_buf;
     id_t input_id_buf;
+    
+    //     | | |  (input)
+    //   input_buf    
+    // r1| |r2 | | \r3
+    //   mul   | |  |
+    //    buf  | |  |
+    //    |  \ | |  |
+    //    |   sgn-mod  (comb. selects 2 inputs (mul+rs3 / rs1+rs2). Can SignMod first operand, can invert second operand. First operand is also available as buffered result for sgn-ops).
+    //    |   bufA bufB
+    //    |  /   \  |           
+    //    | |   adder  (also subtracts if 2nd operand was inverted above)
+    //    | |    buf
+    //    | |   /
+    //   result (currently combinatorial)
+    //     |  (output)
 
-    localparam MUL_STAGES = 3; // including in & out buffers // code below relies on min 2 stages
+
+    localparam MUL_STAGES = 2; // only internal to MUL & output buffer
 
     logic valid_mul [MUL_STAGES];
     stage_state_t state_mul [MUL_STAGES];
 
-    localparam ADD_STAGES = 4; // including in & out buffers // code below relies on min 2 stages
+    localparam ADD_STAGES = 4; // including in buffers (which include sign-mod logic) & outputbuffer
 
     logic valid_add [ADD_STAGES];
     stage_state_t state_add [ADD_STAGES];
 
+	/////////////////////////////// Input Stage
 
     assign issue.ready = input_advance;
-    assign input_advance = !valid_input || (input_buf.op.mul && mul_advance) || (!input_buf.op.mul && add_advance);
+    logic input_out_taken;
+    assign input_advance = !valid_input || input_out_taken;
 
     always_ff @(posedge clk) begin
         if (rst) begin
@@ -53,7 +72,7 @@ module fp_mac_unit_sp
         end
     end
 
-    flopoco_t mulInputA_r, mulInputB_r;
+    /////////////////////// Mul Stage
 
     flopoco_t mulResult;
     flopoco_t mulResult_r;
@@ -64,6 +83,8 @@ module fp_mac_unit_sp
     assign start_mul = valid_input && input_buf.op.mul;
     logic mul_out_taken;
     assign mul_advance = !valid_mul[MUL_STAGES-1] || mul_out_taken;
+    logic mul_takes_input;
+    assign mul_takes_input = input_buf.op.mul && mul_advance;
 
     always_ff @(posedge clk) begin
         if (rst) begin
@@ -75,8 +96,6 @@ module fp_mac_unit_sp
                 valid_mul[i] <= valid_mul[i-1];
         end
         if (mul_advance) begin
-            mulInputA_r <= input_buf.rs1;
-            mulInputB_r <= input_buf.rs2;
             mul_rs3_buf[0] <= input_buf.rs3;
             state_mul[0] <= '{ op: input_buf.op, id: input_id_buf};
             for (int i=1; i < MUL_STAGES; i++) begin
@@ -90,10 +109,20 @@ module fp_mac_unit_sp
     f_mul mul( // 1cyc, (needs input + output buffers)
         .clk(clk),
         .ce(mul_advance),
-        .X(mulInputA_r),
-        .Y(mulInputB_r),
+        .X(input_buf.rs1),
+        .Y(input_buf.rs2),
         .R(mulResult)
     );
+
+    logic final_mul;
+	flopoco_t final_mul_result;
+	id_t final_mul_id;
+	assign final_mul = valid_mul[MUL_STAGES-1] && state_mul[MUL_STAGES-1].op.add_mul_rs3 == 0;
+	//assign final_mul_result = mulResult;
+	assign final_mul_result = mulResult_r;
+    assign final_mul_id = state_mul[MUL_STAGES-1].id;
+
+	//////////////////// SignMod & Add
 
     logic addMode_mulResult;
     logic start_add_in;
@@ -115,6 +144,9 @@ module fp_mac_unit_sp
     sign_mod_t ovr_sign_mode;
     assign ovr_sign_mode = addMode_mulResult? state_mul[MUL_STAGES-1].op.ovr_1st_add_in_sign : input_buf.op.ovr_1st_add_in_sign;
     logic will_need_adder;
+    logic signMod_takes_input;
+    //assign signMod_takes_input = !input_buf.op.add_rs1_rs2 && !input_buf.op.mul && result_takes_signMod;
+    assign signMod_takes_input = !input_buf.op.mul && signMod_advance;
 
     always_comb begin
         case (ovr_sign_mode)
@@ -138,33 +170,47 @@ module fp_mac_unit_sp
         end
     end
 
-    logic addIn_out_taken;
-    assign addIn_advance = !valid_add[0] || addIn_out_taken;
+    logic signMod_out_taken;
+    assign signMod_advance = !valid_add[0] || signMod_out_taken;
+    //assign signMod_advance = add_advance;
     logic valid_for_adder;
+    //logic add_takes_input;
+    //assign add_takes_input = input_buf.op.add_rs1_rs2 && signMod_advance;
 
     always_ff @(posedge clk) begin
         if (rst) begin
             valid_add[0] <= 0;
             valid_for_adder <= 0;
-        end else if (addIn_advance) begin
+        end else if (signMod_advance) begin
             valid_add[0] <= start_add_in;
             valid_for_adder <= will_need_adder;
         end
-        if (addIn_advance) begin
+        if (signMod_advance) begin
             adderInputA_r <= adderInputA;
             adderInputB_r <= adderInputB;
             state_add[0] <= addMode_mulResult? state_mul[MUL_STAGES-1] : '{ op: input_buf.op, id: input_id_buf};
         end
     end
 
-    logic add_takes_addIn;
+    logic final_signMod;
+	flopoco_t final_signMod_result;
+	id_t final_signMod_id;
+	//assign final_signMod = valid_input && !input_buf.op.mul && !input_buf.op.add_rs1_rs2; // only signMod result if there was NO other op at all
+	assign final_signMod = valid_add[0] && !valid_for_adder; // only signMod result if there was NO other op at all
+	//assign final_signMod_result = adderInputA;
+	assign final_signMod_result = adderInputA_r;
+	//assign final_signMod_id = input_id_buf;
+	assign final_signMod_id = state_add[0].id;
+
+
+    logic add_takes_signMod;
     logic add_out_taken;
     assign add_advance = !valid_add[ADD_STAGES-1] || add_out_taken;
-    assign add_takes_addIn = valid_for_adder;
+    assign add_takes_signMod = valid_for_adder;
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            for (int i=1; i < ADD_STAGES; i++)
+            for (int i=1; i < ADD_STAGES; i++) // skips stage0. handled separately above, because it is sgn-mod AND adder-input simultaneously
                 valid_add[i] <= 0;
         end else if (add_advance) begin
             valid_add[1] <= valid_for_adder; // only continue if we actually need to add
@@ -189,15 +235,29 @@ module fp_mac_unit_sp
     flopoco_t result;
     id_t result_id;
 
-    logic final_add, final_mul, final_signMod;
-    assign final_mul = valid_mul[MUL_STAGES-1] && state_mul[MUL_STAGES-1].op.add_mul_rs3 == 0;
+  	logic final_add;
+	flopoco_t final_add_result;
+	id_t final_add_id;
     assign final_add = valid_add[ADD_STAGES-1];
-    assign final_signMod = valid_add[0] && !valid_for_adder; // only signMod result if there was NO other op at all
+	//assign final_add_result = adderResult;
+	assign final_add_result = adderResult_r;
+	assign final_add_id = state_add[ADD_STAGES-1].id;
+
+	/////////////////////// Result Stage
 
     logic result_takes_mul, result_takes_add, result_takes_signMod;
     assign mul_out_taken = add_takes_mul || result_takes_mul;
     assign add_out_taken = result_takes_add;
-    assign addIn_out_taken = result_takes_signMod || add_takes_addIn;
+    assign signMod_out_taken = result_takes_signMod || add_takes_signMod;
+    //assign input_out_taken = mul_takes_input || add_takes_input || signMod_takes_input;
+    assign input_out_taken = mul_takes_input || signMod_takes_input;
+
+    flopoco_t shared_result;
+    //flopoco_t shared_result_r;
+    id_t shared_result_id;
+    //id_t shared_result_id_r;
+    logic valid_shared_result;
+    //logic valid_shared_result_r;
 
     always_comb begin
         result_takes_mul = 0;
@@ -205,25 +265,46 @@ module fp_mac_unit_sp
         result_takes_signMod = 0;
 
         if (final_add) begin
-            result = adderResult_r;
-            result_id = state_add[ADD_STAGES-1].id;
-            result_takes_add = wb.ack;
+            shared_result = final_add_result;
+            shared_result_id = final_add_id;
+            result_takes_add = result_advance;
         end else if (!final_signMod) begin // mul, but give signMod preference while keeping it default
-            result = mulResult_r;
-            result_id = state_mul[MUL_STAGES-1].id;
-            result_takes_mul = wb.ack;
+            shared_result = final_mul_result;
+            shared_result_id = final_mul_id;
+            result_takes_mul = result_advance;
         end else begin // signMod
-            result = adderInputA_r;
-            result_id = state_add[0].id;
-            result_takes_signMod = wb.ack;
+            shared_result = final_signMod_result;
+			shared_result_id = final_signMod_id;
+            result_takes_signMod = result_advance;
         end
     end
+
+    assign valid_shared_result = final_add || final_signMod || final_mul;
+
+    //assign result_advance = !valid_shared_result_r || wb.ack;
+    assign result_advance = wb.ack;
+
+    //always_ff @(posedge clk) begin
+    //    if (rst)
+    //        valid_shared_result <= 0;
+    //    else if (result_advance)
+    //        valid_shared_result_r <= valid_shared_result;
+    //    if (result_advance) begin
+    //        shared_result_r <= shared_result;
+    //        shared_result_id_r <= shared_result_id;
+    //    end
+    //end
 
     //WB interface
     ////////////////////////////////////////////////////
 
-    assign wb.rd = result;
-    assign wb.done = final_add || final_signMod || final_mul;
-    assign wb.id = result_id;
+	// variant with shared result buffer
+    //assign wb.rd = shared_result_r;
+    //assign wb.done = valid_shared_result;
+    //assign wb.id = shared_result_id_r;
+
+    assign wb.rd = shared_result;
+	assign wb.done = valid_shared_result;
+	assign wb.id = shared_result_id;
 
 endmodule : fp_mac_unit_sp
